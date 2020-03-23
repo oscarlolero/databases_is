f3db06f2e34d8c788a5f2fa9d1cfbd2f3c445f79
Oscar Montes <oscmcojc1@live.com.mx>
2020-03-23 15:40:19 -0600

    Store script identation fix

--- Store/store.sql
+++ Store/store.sql
@@ -5,25 +5,25 @@ CREATE TABLE Products (
 	product_id VARCHAR(15) NOT NULL,
 	price INT,
 	descr VARCHAR(255),
-    image VARCHAR(255),
-    PRIMARY KEY (product_id)
+	image VARCHAR(255),
+	PRIMARY KEY (product_id)
 );
 CREATE TABLE Clients (
 	client_id VARCHAR(15) NOT NULL,
 	username VARCHAR(255),
 	password VARCHAR(255),
 	email VARCHAR(255),
-    phone VARCHAR(50),
-    address VARCHAR(255)
+	phone VARCHAR(50),
+	address VARCHAR(255)
 );
 
 CREATE TABLE Orders (
 	order_id VARCHAR(15) NOT NULL,
 	product_id VARCHAR(15) NOT NULL,
-    total INT,
-    ship_address VARCHAR(255),
-    shipping_date DATE,
-    delivery_date DATE,
+	total INT,
+	ship_address VARCHAR(255),
+	shipping_date DATE,
+	delivery_date DATE,
 	PRIMARY KEY (order_id),
 	FOREIGN KEY (product_id) REFERENCES Products(product_id)
 )
